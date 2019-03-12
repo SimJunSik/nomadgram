afef15b7da4c388bf350c4adc0d100c689c763d0
SimJunSik <wnstlr24@naver.com>
2019-03-12 15:04:30 +0900

    end test

--- nomadgram/images/models.py
+++ nomadgram/images/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from nomadgram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
 # Create your models here.
+@python_2_unicode_compatible
 class TimeStampedModel(models.Model) :
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,22 +12,32 @@ class TimeStampedModel(models.Model) :
     class Meta :
         abstract = True
 
-
+@python_2_unicode_compatible
 class Image(TimeStampedModel) :
 
     file = models.ImageField()
     location = models.CharField(max_length = 140)
-    captions = models.TextField()
+    caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
 
+    def __str__(self) :
+        return '{} - {}'.format(self.location, self.caption)
+
+@python_2_unicode_compatible
 class Comment(TimeStampedModel) :
 
     message = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE, related_name='comments')
 
+    def __str__(self) :
+        return self.message
 
+@python_2_unicode_compatible
 class Like(TimeStampedModel) :
 
     creator = models.ForeignKey(user_models.User, null=True, on_delete=models.CASCADE)
-    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, null=True, on_delete=models.CASCADE, related_name='likes')
+
+    def __str__(self) :
+        return '{} {}'.format(self.creator.username, self.image.caption)
--- nomadgram/images/views.py
+++ nomadgram/images/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from . import models , serializers
 
 # Create your views here.
+class ListAllImages(APIView) :
+
+    def get(self, request, format=None) :
+
+        all_images = models.Image.objects.all()
+
+        serializer = serializers.ImageSerializers(all_images, many=True)
+
+        return Response(data=serializer.data)
+
+
+class ListAllComments(APIView) :
+
+    def get(self, request, format=None) :
+
+        user_id = request.user.id
+
+        all_comments = models.Comment.objects.filter(creator = user_id)
+
+        serializer = serializers.CommentSerializers(all_comments, many=True)
+
+        return Response(data=serializer.data)
+
+
+class ListAllLikes(APIView) :
+
+    def get(self, request, format=None) :
+
+        all_likes = models.Like.objects.all()
+
+        serializer = serializers.LikeSerializers(all_likes, many=True)
+
+        return Response(data=serializer.data)
